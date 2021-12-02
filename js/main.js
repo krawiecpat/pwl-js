ea8500807f5ac880cc506917c84751a518b05747
krawiecpat <krawiecpat9@gmail.com>
2021-12-02 18:01:32 +0100

    pwl js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let num1 = 2;
+let num2 = 5;
+
+function number(number1, number2) {
+    let wynik = Math.max(number1, number2);
+    return wynik;
+} console.log(number(num1,num2));
+
+console.log(number(88,543));
+console.log(number(423,4));
